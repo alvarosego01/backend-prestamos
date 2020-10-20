93feb2e2bcaf42acd9d8aac5101b74d7caf133c2
Teddy7595 <teddypottella@gmail.com>
2020-10-20 14:15:26 -0400

    *PRE_ALFA 0.1.12 ADMIN/USUARIOS/ROLES/COBRADORES --MODULOS NUEVOS-- *COBRADORES --MODULOS CREADOS-- *ADMINISTRADORES *USUARIOS *ROLES --TEMAS PENDIENTES-- *Los roles estan configurados aunque mantienen ptos pendiente *Falta la configuracion de los middleware para USUARIOS *Falta reloj de licencias (gestionador de tiempo) *Falta macro para setteo de licencias "indeterminadas" en tiempo *Detectado falla de la bitácora administrativa, aplicando cambios... *Falta cambiar roles al settear una licencia *Falta prueba debug al sistema de bitácora/licencias --TEMAS LISTOS-- *Listo cambio de DTO a Interface, Bitacora *Listo CRUD general de licencias *Listo DTO de licencias *Listo status de las licencias *Listo Schema de licencias *Listo DTO de usuario *Listo DTO de bitacora *Listo GET,POST, PUT, DELETE de usuarios *Listo cambio de roles mediante administración *Listo obtención de usuarios en administración *Listo obtención de roles en administración *Listo Bitacora administrativa

--- src/Modules/admin/services/adminLic.service.ts
+++ src/Modules/admin/services/adminLic.service.ts
@@ -3,7 +3,7 @@ import { responseInterface, _configPaginator, _argsPagination, _dataPaginator, _
 import { ProcessDataService, DateProcessService } from 'src/Classes/classes.index';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { LicenseDto, LicenseChangeStatusDto, BitacoraDto  } from '../models/dto/dto.index';
+import { LicenseDto, LicenseChangeStatusDto } from '../models/dto/dto.index';
 import { License } from '../models/schemas/licenseSchema';
 
 @Injectable()
