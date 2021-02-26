89528f153ff8f91f85c4b3c4ee5e75043d9fa195
Alvaro Segovia <alvarosego01@gmail.com>
2021-02-26 16:42:01 -0400

    arreglando fallos

--- src/Modules/clientes/controllers/clientes.controller.ts
+++ src/Modules/clientes/controllers/clientes.controller.ts
@@ -25,7 +25,6 @@ export class ClientesController
         return res.status(200).json("Ruta de manejo de clientes activa");
     }
 
-<<<<<<< HEAD
     @RolesDecorator('ADMIN_ROLE','ENRUTATOR_ROLE')
     @UseGuards(AuthGuard('jwt'), RoleGuard, SameUserAuthGuard)
     @Post('crearSingle/:id')//tomo formulario y creo un cliente
@@ -37,16 +36,16 @@ export class ClientesController
         return res.status(this._Response.status).json(this._Response);
     }
 
-    @RolesDecorator('ADMIN_ROLE','ENRUTATOR_ROLE')
-    @UseGuards(AuthGuard('jwt'), RoleGuard, SameUserAuthGuard)
-    @Post('crear/:id/:idRuta')//tomo formulario y creo un cliente
-    async createNewCliente(@Param('idRuta') idRuta:string, @Param('id') id:string , @Body() body:ClienteDto, @Response() res:any):Promise<responseInterface>
-    {
-        // console.log('la puta madre');
-        // return res.status(200).json("aaaaa");
-        this._Response = await this._clienteService.createNewCliente(body, idRuta);
-        return res.status(this._Response.status).json(this._Response);
-    }
+    // @RolesDecorator('ADMIN_ROLE','ENRUTATOR_ROLE')
+    // @UseGuards(AuthGuard('jwt'), RoleGuard, SameUserAuthGuard)
+    // @Post('crear/:id/:idRuta')//tomo formulario y creo un cliente
+    // async createNewCliente(@Param('idRuta') idRuta:string, @Param('id') id:string , @Body() body:ClienteDto, @Response() res:any):Promise<responseInterface>
+    // {
+    //     // console.log('la puta madre');
+    //     // return res.status(200).json("aaaaa");
+    //     this._Response = await this._clienteService.createNewCliente(body, idRuta);
+    //     return res.status(this._Response.status).json(this._Response);
+    // }
 
     @RolesDecorator('ADMIN_ROLE','ENRUTATOR_ROLE')
     @UseGuards(AuthGuard('jwt'), RoleGuard)
@@ -66,10 +65,34 @@ export class ClientesController
         return res.status(this._Response.status).json(this._Response);
     }
 
-=======
+
+    @Get('allByEnrouter/:id')//obtengo el cliente perteneciente al cobrador
+    async getClientsByEnrouter(@Param() params:string[], @Response() res:any):Promise<responseInterface>
+    {
+        this._Response = await this._clienteService.getClientsByEnrouter( params['id'] );
+        return res.status(this._Response.status).json(this._Response);
+    }
+
+    @Get('getCliente/:id')//obtengo el cliente perteneciente al cobrador
+    async getOneClienteById(@Param() params:string[], @Response() res:any):Promise<responseInterface>
+    {
+        this._Response = await this._clienteService.getOneClienteById( params['id'] );
+        return res.status(this._Response.status).json(this._Response);
+    }
+
+
+    // @RolesDecorator('ADMIN_ROLE','ENRUTATOR_ROLE')
+    // @UseGuards(AuthGuard('jwt'), RoleGuard, SameUserAuthGuard)
+    // @Delete('borrar/:id/:cliente')//tomo el id del cliente y lo borro, pero bajo confirmacion del admin
+    // async delOneCliente(@Param('cliente') cliente:string, @Response() res:any):Promise<responseInterface>
+    // {
+    //     this._Response = await this._clienteService.delOneCliente(cliente);
+    //     return res.status(this._Response.status).json(this._Response);
+    // }
+
+
     @RolesDecorator('ADMIN_ROLE', 'ENRUTATOR_ROLE')
     @UseGuards(AuthGuard(), RoleGuard)
->>>>>>> teddy
     @Get('all/:cobrador')//obtengo los cliente pertenecientes al cobrdor
     async getAllClientes(@Param('cobrador') cobrador:string, @Response() res:any):Promise<responseInterface>
     {
@@ -77,74 +100,120 @@ export class ClientesController
         return res.status(this._Response.status).json(this._Response);
     }
 
-<<<<<<< HEAD
-    @Get('allByEnrouter/:id')//obtengo el cliente perteneciente al cobrador
-    async getClientsByEnrouter(@Param() params:string[], @Response() res:any):Promise<responseInterface>
-=======
 
     @RolesDecorator('ADMIN_ROLE', 'ENRUTATOR_ROLE', 'COLLECTOR_ROLE')
     @UseGuards(AuthGuard(), RoleGuard)
     @Get(':cliente/:cobrador')//obtengo el cliente perteneciente al cobrador
     async getOneCliente(@Param() params:string[], @Response() res:any):Promise<responseInterface>
->>>>>>> teddy
-    {
-        this._Response = await this._clienteService.getClientsByEnrouter( params['id'] );
-        return res.status(this._Response.status).json(this._Response);
-    }
-
-<<<<<<< HEAD
-    @Get('getCliente/:id')//obtengo el cliente perteneciente al cobrador
-    async getOneClienteById(@Param() params:string[], @Response() res:any):Promise<responseInterface>
-=======
-    @RolesDecorator('ADMIN_ROLE', 'ENRUTATOR_ROLE', 'ENRUTATOR_ROLE')
-    @UseGuards(AuthGuard(), RoleGuard)
-    @Post('crear/:cobrador')//tomo formulario y creo un cliente
-    async createNewCliente(@Param('cobrador') cobrador:string, @Body() body:ClienteDto, @Response() res:any):Promise<responseInterface>
->>>>>>> teddy
-    {
-        this._Response = await this._clienteService.getOneClienteById( params['id'] );
-        return res.status(this._Response.status).json(this._Response);
-    }
-
-<<<<<<< HEAD
-    @Get(':cliente/:cobrador')//obtengo el cliente perteneciente al cobrador
-    async getOneCliente(@Param() params:string[], @Response() res:any):Promise<responseInterface>
-=======
-    @RolesDecorator('ADMIN_ROLE', 'ENRUTATOR_ROLE')
-    @UseGuards(AuthGuard(), RoleGuard)
-    @Delete('borrar/:cliente')//tomo el id del cliente y lo borro, pero bajo confirmacion del admin
-    async delOneCliente(@Param('cliente') cliente:string, @Response() res:any):Promise<responseInterface>
->>>>>>> teddy
     {
         this._Response = await this._clienteService.getOneCliente(params['cliente'], params['cobrador']);
         return res.status(this._Response.status).json(this._Response);
     }
 
-<<<<<<< HEAD
+    @RolesDecorator('ADMIN_ROLE', 'ENRUTATOR_ROLE', 'ENRUTATOR_ROLE')
+    @UseGuards(AuthGuard(), RoleGuard)
+    @Post('crear/:cobrador')//tomo formulario y creo un cliente
+    async createNewCliente(@Param('cobrador') cobrador:string, @Body() body:ClienteDto, @Response() res:any):Promise<responseInterface>
+    {
+        this._Response = await this._clienteService.createNewCliente(body, cobrador);
+        return res.status(this._Response.status).json(this._Response);
+    }
 
-
-
-
-    @RolesDecorator('ADMIN_ROLE','ENRUTATOR_ROLE')
-    @UseGuards(AuthGuard('jwt'), RoleGuard, SameUserAuthGuard)
-    @Delete('borrar/:id/:cliente')//tomo el id del cliente y lo borro, pero bajo confirmacion del admin
-    async delOneCliente(@Param('cliente') cliente:string, @Response() res:any):Promise<responseInterface>
-=======
     @RolesDecorator('ADMIN_ROLE', 'ENRUTATOR_ROLE')
     @UseGuards(AuthGuard(), RoleGuard)
-    @Post('enlazar/:cliente/:ruta')//tomo el id del cliente y el id de la ruta y lo enlazo
-    async linkToRouteOneCliente(@Param() params:string[], @Body() enlace:any, @Response() res:any):Promise<responseInterface>
->>>>>>> teddy
+    @Delete('borrar/:cliente')//tomo el id del cliente y lo borro, pero bajo confirmacion del admin
+    async delOneCliente(@Param('cliente') cliente:string, @Response() res:any):Promise<responseInterface>
     {
         this._Response = await this._clienteService.delOneCliente(cliente);
         return res.status(this._Response.status).json(this._Response);
     }
 
-    // @Post('enlazar/:cliente/:ruta')//tomo el id del cliente y el id de la ruta y lo enlazo
-    // async linkToRouteOneCliente(@Param() params:string[], @Body() enlace:any, @Response() res:any):Promise<responseInterface>
-    // {
-    //     this._Response = await this._rutaClienteService.linkToRouteOneCliente(params['ruta'], params['cliente']);
-    //     return res.status(this._Response.status).json(this._Response);
-    // }
+    @RolesDecorator('ADMIN_ROLE', 'ENRUTATOR_ROLE')
+    @UseGuards(AuthGuard(), RoleGuard)
+    @Post('enlazar/:cliente/:ruta')//tomo el id del cliente y el id de la ruta y lo enlazo
+    async linkToRouteOneCliente(@Param() params:string[], @Body() enlace:any, @Response() res:any):Promise<responseInterface>
+    {
+        this._Response = await this._rutaClienteService.linkToRouteOneCliente(params['ruta'], params['cliente']);
+        return res.status(this._Response.status).json(this._Response);
+    }
 
 }
+
+
+
+
+
+
+// import { Controller, Response, Get, Post, Body, Param, Delete, UseGuards } from '@nestjs/common';
+// import { responseInterface } from 'src/Response/interfaces/interfaces.index';
+// import { ClienteDto } from '../models/dto/index.dto';
+// import { ClienteService, RutaClienteService } from '../services/services.index';
+
+// import { AuthGuard, PassportModule } from '@nestjs/passport';
+// import {RolesDecorator} from "src/Modules/role/decorators/role.decorator";
+// import {RoleGuard} from "src/Modules/role/guards/roleGuard.index";
+
+// @Controller('clientes')
+// export class ClientesController
+// {
+//     private _Response:responseInterface;
+
+//     constructor
+//     (
+//         private _clienteService:ClienteService,
+//         private _rutaClienteService:RutaClienteService
+//     ){}
+
+//     @Get('hello')
+//     async sayHello(@Response() res:any):Promise<responseInterface>
+//     {
+//         return res.status(200).json("Ruta de manejo de clientes activa");
+//     }
+
+//     @RolesDecorator('ADMIN_ROLE', 'ENRUTATOR_ROLE')
+//     @UseGuards(AuthGuard(), RoleGuard)
+//     @Get('all/:cobrador')//obtengo los cliente pertenecientes al cobrdor
+//     async getAllClientes(@Param('cobrador') cobrador:string, @Response() res:any):Promise<responseInterface>
+//     {
+//         this._Response = await this._clienteService.getAllClientes(cobrador);
+//         return res.status(this._Response.status).json(this._Response);
+//     }
+
+
+//     @RolesDecorator('ADMIN_ROLE', 'ENRUTATOR_ROLE', 'COLLECTOR_ROLE')
+//     @UseGuards(AuthGuard(), RoleGuard)
+//     @Get(':cliente/:cobrador')//obtengo el cliente perteneciente al cobrador
+//     async getOneCliente(@Param() params:string[], @Response() res:any):Promise<responseInterface>
+//     {
+//         this._Response = await this._clienteService.getOneCliente(params['cliente'], params['cobrador']);
+//         return res.status(this._Response.status).json(this._Response);
+//     }
+
+//     @RolesDecorator('ADMIN_ROLE', 'ENRUTATOR_ROLE', 'ENRUTATOR_ROLE')
+//     @UseGuards(AuthGuard(), RoleGuard)
+//     @Post('crear/:cobrador')//tomo formulario y creo un cliente
+//     async createNewCliente(@Param('cobrador') cobrador:string, @Body() body:ClienteDto, @Response() res:any):Promise<responseInterface>
+//     {
+//         this._Response = await this._clienteService.createNewCliente(body, cobrador);
+//         return res.status(this._Response.status).json(this._Response);
+//     }
+
+//     @RolesDecorator('ADMIN_ROLE', 'ENRUTATOR_ROLE')
+//     @UseGuards(AuthGuard(), RoleGuard)
+//     @Delete('borrar/:cliente')//tomo el id del cliente y lo borro, pero bajo confirmacion del admin
+//     async delOneCliente(@Param('cliente') cliente:string, @Response() res:any):Promise<responseInterface>
+//     {
+//         this._Response = await this._clienteService.delOneCliente(cliente);
+//         return res.status(this._Response.status).json(this._Response);
+//     }
+
+//     @RolesDecorator('ADMIN_ROLE', 'ENRUTATOR_ROLE')
+//     @UseGuards(AuthGuard(), RoleGuard)
+//     @Post('enlazar/:cliente/:ruta')//tomo el id del cliente y el id de la ruta y lo enlazo
+//     async linkToRouteOneCliente(@Param() params:string[], @Body() enlace:any, @Response() res:any):Promise<responseInterface>
+//     {
+//         this._Response = await this._rutaClienteService.linkToRouteOneCliente(params['ruta'], params['cliente']);
+//         return res.status(this._Response.status).json(this._Response);
+//     }
+
+// }
